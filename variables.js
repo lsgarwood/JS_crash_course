682b77139209588ad9aab729f7eb7a3ba42798d0
Lauren <ls_garwood@yahoo.co.uk>
2022-04-15 19:17:36 +0100

    LG: variables

---
+++ variables.js
@@ -0,0 +1,18 @@
+var firstName = "Jamila";
+var age = 21;
+var isFemale = true;
+var balance = 100.33;
+var dob = new Date(2003, 2, 28);
+var person = {}
+var empty = undefined
+
+console.log(firstName);
+console.log(age);
+console.log(isFemale);
+console.log(balance);
+console.log(dob);
+console.log(person);
+console.log(empty);
+
+
+
