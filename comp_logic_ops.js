15d4074396804cfcacb75ecdfb19a044a30353dc
Lauren <ls_garwood@yahoo.co.uk>
2022-04-15 22:39:38 +0100

    LG: comparison and logical operators

---
+++ comp_logic_ops.js
@@ -0,0 +1,22 @@
+//Comparisson Operators
+// < <= > >= == !=
+//boolean is either true or false
+
+console.log(10 == 10);
+console.log(10 != 10);
+console.log(10 < 10);
+console.log(10 <= 10);
+console.log(10 > 10);
+console.log(10 >= 10);
+
+console.log("A" == "B");
+console.log("A" != "B");
+
+//---------------------
+//Logical Operators
+// && and
+// || or
+
+console.log(10 == 10 && "A" == "A");
+console.log(10 == 10 && "A" == "B");
+console.log(10 == 10 || "A" == "B");
