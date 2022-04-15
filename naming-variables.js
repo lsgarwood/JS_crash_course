0825c74d007dfbd405bc9456695da0ccafae4130
Lauren <ls_garwood@yahoo.co.uk>
2022-04-15 19:31:51 +0100

    LG: naming varibales

---
+++ naming-variables.js
@@ -0,0 +1,18 @@
+// Naming
+/*
+Variable names
+- cannot contain spaces
+- must begin with a letter, _ underscore, or $ dollar sign
+- names can only contain letters, numbers, underscores, or dollar signs
+- names are cade sensitive
+- cannot use reserved keywords
+- and have meaning that makes sense, and reperesent the value
+- boolean- start with is
+*/
+var firstName = "Jamila";
+var age = 21;
+var isFemale = true;
+var balance = 100.33;
+var dob = new Date(2003, 2, 28);
+var person = {}
+var empty = undefined
