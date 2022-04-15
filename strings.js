643ce35a699becdd9b06852c6a11f8deffb4d4f2
Lauren <ls_garwood@yahoo.co.uk>
2022-04-15 19:38:31 +0100

    LG: strings

---
+++ strings.js
@@ -0,0 +1,18 @@
+//strings
+
+var brand = "Amigoscode";
+
+console.log(typeof brand);
+console.log(brand.length);
+console.log(brand.toUpperCase());
+console.log(brand.substring(0, 6));
+
+//concatinate
+
+var a = "Amigos"
+var b = "code"
+console.log(a+b);
+console.log(a + " " + b);
+console.log(`${a}${b}`);
+console.log(`${a} ${b}`);
+console.log(`${a} ${b.toUpperCase()}`);
