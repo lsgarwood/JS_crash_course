e326363592086f056a0a6ea1653ecc7c9d3a20db
Lauren <ls_garwood@yahoo.co.uk>
2022-04-15 20:04:10 +0100

    LG:booleans

---
+++ booleans.js
@@ -0,0 +1,21 @@
+//Boolean
+
+var isAdult = false;
+console.log(isAdult);
+
+if (isAdult) { //checking if isAdult is false
+    console.log("is adult")
+} else {
+    console.log("is not an adult") // which it is
+}
+
+console.log(!true); // adding ! meant 'not'true
+console.log(!false);
+
+if (!isAdult) { //adding ! here will make isAdult true
+    console.log("is adult") //now the result is true
+} else {
+    console.log("is not an adult")
+}
+
+
