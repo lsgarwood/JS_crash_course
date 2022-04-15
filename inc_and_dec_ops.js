e634e46582c9fa6df4ffecae5ddcb6935bde739c
Lauren <ls_garwood@yahoo.co.uk>
2022-04-15 22:30:47 +0100

    LG: increment and decrement ops

---
+++ inc_and_dec_ops.js
@@ -0,0 +1,16 @@
+//Increment and decrement operators
+
+var number = 0;
+//postfix
+console.log(number++); //return the number then increment
+console.log(number);
+console.log(number--); //return the number then increment
+console.log(number);
+//prefix
+var numberTwo = 0;
+console.log(++numberTwo); // increment then return number
+console.log(--numberTwo); // decrement then return the number
+//for loop
+for (var i = 0; i<=10; i++) { //return the number then increment
+    console.log(i);
+}
