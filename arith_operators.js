22c3e6dcfbf7870b79d82419ee76d28e495267b6
Lauren <ls_garwood@yahoo.co.uk>
2022-04-15 21:11:53 +0100

    LG: arithmetic operators

---
+++ arith_operators.js
@@ -0,0 +1,14 @@
+//Arithmetic Operators
+var addition = 2 + 2;
+var subtration = 2 - 2;
+var division = 10 / 2;
+var multiply = 10 * 2;
+var remainder = 10 % 9;
+var exponentation = 3 ** 4; // 3x3x3x3
+
+console.log(addition);
+console.log(subtration);
+console.log(division);
+console.log(multiply);
+console.log(remainder);
+console.log(exponentation);
