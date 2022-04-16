dc0b15aae5b59d4e8e6c4362fcdfeb3d83fa6896
Lauren <ls_garwood@yahoo.co.uk>
2022-04-16 08:54:52 +0100

    LG: if statements 1

---
+++ ifstatements.js
@@ -0,0 +1,21 @@
+//If Statements
+
+// run a piece of code depending on a condition
+
+var condition = 1 > 0;
+
+if(condition) {
+    console.log("Runs....");
+} else {
+    console.log("Else conditin ran...");
+}
+
+var gender = "Hello";
+
+if (gender == "M") {
+    console.log("Male");
+} else if (gender == "F") {
+    console.log("Female");
+} else {
+    console.log("unknown");
+}
